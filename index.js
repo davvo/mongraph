11b109941074adce52ca30e0df607766d432d4c7
David Ershag <david.ershag@gmail.com>
2014-04-16 16:59:05 +0200

    worked on it

--- index.js
+++ index.js
@@ -1,14 +1,81 @@
-var mongodb = require('mongodb');
+var levelup = require('levelup'),
+    parseDuration = require('parse-duration'),
+    Args = require('args-js'),
+    db = levelup('./db'),
+    receiver = require('./lib/receiver')(db);
 
-mongodb.MongoClient.connect(process.env.MONGO_URL, function (err, db) {
+var port = process.argv[2] || 8125;
+receiver.listen(port);
+console.log("Receiver listening on port ", port);
 
-    if (err) {
-        return console.error("Could not connect to mongo ", err);
+function makeDense(metrics, start, end, duration) {
+    var count = 0;
+    var dense = {};
+    var time = Math.floor(end / duration) * duration;
+    while (time >= start && ++count < 10000) {
+        dense[time] = metrics[time] || 0;
+        time -= duration;        
     }
+    return dense;
+}
 
-    db.collectionNames(function (err, names) {
-        console.log(names);
-        db.close();
+function getStartDate() {
+    var date = new Date();
+    date.setDate(date.getDate() - 1);
+    return date.getTime();
+}
+
+function getEndDate() {
+    var date = new Date();
+    return date.getTime();
+}
+
+function getMetrics() {
+    var args = Args([
+        {name: Args.STRING | Args.Required},
+        {start: Args.INT | Args.Optional, _default: getStartDate()},
+        {end: Args.INT | Args.Optional, _default: getEndDate()},
+        {agg: Args.STRING | Args.Optional, _default: '1 min'},
+        {dense: Args.BOOL | Args.Optional, _default: false},
+        {done: Args.FUNCTION | Args.Required},
+    ], arguments);
+
+    var metrics = {},
+        duration = parseDuration(args.agg);
+    db.createReadStream({
+        start: args.name + '.' + args.start,
+        end: args.name + '.' + args.end
+    }).on('error', function (err) {
+        args.done(err);
+    }).on('data', function (data) {
+        var millis = parseInt(data.key.substring(data.key.lastIndexOf('.') + 1), 10),
+            key = Math.floor(millis / duration) * duration;
+        metrics[key] = parseFloat(data.value) + (metrics[key] || 0);
+    }).on('end', function () {
+        if (args.dense) {
+            metrics = makeDense(metrics, args.start, args.end, duration);
+        }
+        args.done(null, Object.keys(metrics).map(function (key) {
+            return {
+                time: parseInt(key, 10),
+                value: metrics[key]
+            };
+        }).sort(function (a, b) {
+            return a.time - b.time;
+        }));
     });
+}
 
-});
+setTimeout(function () {
+    getMetrics({
+        name: 'foo.bar.baz',
+        done: function (err, metrics) {
+            if (err) {
+                throw err;
+            }
+            console.log(metrics.length);
+        },
+        agg: '1min',
+        dense: true
+    });
+}, 1000);
---
+++ test/testClient.js
@@ -0,0 +1,20 @@
+var net = require('net');
+
+function getData() {
+    var key = 'foo.bar.baz',
+        value = Math.floor(Math.random() * 100),
+        time = new Date().getTime();
+    return [key, value, time].join(' ') + '\n';
+}
+
+var client = net.connect({port: 8125}, function () {
+    (function send() {
+        var i, data = [];
+        for (i = 0; i < Math.random() * 10; ++i) {
+            data.push(getData());
+        }
+        console.log(data.join(''));
+        client.write(data.join(''));
+        setTimeout(send, Math.random() * 3000);
+    }());
+});
